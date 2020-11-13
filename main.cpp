f167614c4a9a3545160087a32c635ed33ba0cccd
Akutagawa50 <ryusuke0525a@gmail.com>
2020-11-13 13:02:40 +0900

    文中の文字の数を出力するようにした

--- main.cpp
+++ main.cpp
@@ -10,7 +10,20 @@ char CapsTrans(char c){     //大文字を小文字に変換する関数
     return c;
 }
 
+int AlphaTrans(char c){     //アルファベット(小文字)をaを0としたときの番号を返す関数
+    return (int)c - 97;
+}
+
+char NumberTrans(int x){    //0をaとしたときのアルファベットを返す関数
+    return x==26? ' ':(char)(x+97);
+}
+
+
+
 int main(){
+    int alphacnt[27]={0};
+    int soatalpha[27][2]={0};
+
     ifstream ifs;
     ofstream ofs;
     ifs.open("originaltext.txt");               //入力用ファイルを開く
@@ -20,13 +33,24 @@ int main(){
 
     char c;
     while((c=ifs.get()) != EOF){
-        if(isalpha(c) || c == '\n')
-            ofs << CapsTrans(c);
-        else
+        if(isalpha(c)){
+            c=CapsTrans(c);             //小文字に変換
+            alphacnt[AlphaTrans(c)]++;  //出てきたアルファベットをカウント
+            ofs << c;
+        }
+        else if(c=='\n')
+            ofs << c;
+        else{
+            alphacnt[26]++; //spaceをカウント
             ofs <<' ';
+        }
         
     }
 
+    for(int i=0; i<27; i++){
+        //cout << "'" << NumberTrans(i) << "': " << alphacnt[i] << endl;
+        cout << alphacnt[i] << ": '" << NumberTrans(i) << "'" << endl;
+    }
     return 0;
 }
 
